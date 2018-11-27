293422c1519a195a40617a5abe367a0f42979f0e
rajp5210 <45402615+rajp5210@users.noreply.github.com>
2018-11-27 14:30:25 -0500

    Update flagwaver.js

--- flagwaver.js
+++ flagwaver.js
@@ -285,54 +285,6 @@
             }
         }
 
-        // Bend
-
-        // var wlen = restDistance * 2,
-        //     hlen = restDistance * 2;
-
-        // diagonalDist = window.Math.sqrt( wlen * wlen + hlen * hlen );
-
-        // for ( v = 0; v < ySegs - 1; v++ ) {
-        //     for ( u = 0; u < xSegs - 1; u++ ) {
-
-        //         constraints.push( new Constraint(
-        //             particles[ index( u, v ) ],
-        //             particles[ index( u + 2, v ) ],
-        //             wlen
-        //         ) );
-
-        //         constraints.push( new Constraint(
-        //             particles[ index( u, v ) ],
-        //             particles[ index( u, v + 2 ) ],
-        //             hlen
-        //         ) );
-
-        //         constraints.push( new Constraint(
-        //             particles[ index( u, v ) ],
-        //             particles[ index( u + 2, v + 2 ) ],
-        //             diagonalDist
-        //         ) );
-
-        //         constraints.push( new Constraint(
-        //             particles[ index( u, v + 2 ) ],
-        //             particles[ index( u + 2, v + 2 ) ],
-        //             wlen
-        //         ) );
-
-        //         constraints.push( new Constraint(
-        //             particles[ index( u + 2, v + 2 ) ],
-        //             particles[ index( u + 2, v + 2 ) ],
-        //             hlen
-        //         ) );
-
-        //         constraints.push( new Constraint(
-        //             particles[ index( u + 2,  v ) ],
-        //             particles[ index( u , v + 2 ) ],
-        //             diagonalDist
-        //         ) );
-
-        //     }
-        // }
 
         // Public properties and methods
         this.xSegs        = xSegs;
@@ -441,8 +393,8 @@
 
     // Default flag options
     var defaultOptions = {
-        width         : 300,
-        height        : 200,
+        width         : 600,
+        height        : 400,
         mass          : MASS,
         levelOfDetail : 10
     };
